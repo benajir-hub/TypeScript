0b4c022e63c1b7ace30e26bf94d9d61c22655089
benajir-hub <benajir.shuvo@gmail.com>
2023-03-15 19:30:02 +0100

    TypeScript Variable Practice

---
+++ ts-variable/main.js
@@ -0,0 +1,18 @@
+//var number = 1;
+//let count = 2;
+/*using VAR keyword*/
+function doSomethingRight() {
+    for (var i = 0; i < 6; i++) {
+        console.log(i);
+    }
+    console.log('Finally' + i); //i is valid inside the function
+}
+doSomethingRight();
+/*Using LET Keyword*/
+function doSomething() {
+    for (var i = 0; i < 6; i++) {
+        console.log(i);
+    }
+    console.log('Finally' + i); // i is valid inside the block
+}
+doSomething();
