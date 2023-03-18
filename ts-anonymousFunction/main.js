6f8a9c1022b47a2a5d3f2f0e346be3860eae7e2e
benajir-hub <benajir.shuvo@gmail.com>
2023-03-18 08:49:00 +0100

    Anonymous function with data type and return type

---
+++ ts-anonymousFunction/main.js
@@ -0,0 +1,6 @@
+// Anonymous function  
+var myAdd = function (x, y) {
+    return x + y;
+};
+// Anonymous function call  
+console.log(myAdd(5, 3));
---
+++ ts-anonymousFunction/main.ts
@@ -0,0 +1,6 @@
+// Anonymous function  
+let myAdd = function (x: number, y: number) : number {  
+    return x + y;  
+};  
+// Anonymous function call  
+console.log(myAdd(5,3));  
