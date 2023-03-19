f596be85a9d16be00024c72b15cbd842bb82f0c1
benajir-hub <benajir.shuvo@gmail.com>
2023-03-19 13:17:50 +0100

    Inline Type Annotation practice

---
+++ ts-inlineTypeAnnotation/main.js
@@ -0,0 +1,16 @@
+/**In TypeScript, inline type annotations allow us to declare an object for each of the properties of the object */
+var student;
+//Above we declare an object student with two properties 'id' and 'name' with the data type number and string
+student = {
+    id: 101,
+    name: 'Sam'
+};
+//Assigning value to the properties of student
+student = {
+    id: 102,
+    name: 'Dom'
+};
+//printing latest student object
+console.log(student);
+//printing the latest values of student object
+console.log('Student ID: ' + student.id + '\n' + 'Student Name:' + student.name);
