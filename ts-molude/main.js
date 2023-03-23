a3e78773e140665fdb5561fbc61359d557ce137c
benajir-hub <benajir.shuvo@gmail.com>
2023-03-23 07:55:24 +0100

    Renamed the module folder of TypeScript

--- ts-molude/main.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-//Importing 'Point' class from './point' which is the name of the module or the path
-var point_1 = require("./point");
-var point = new point_1.Point(2, 3);
-point.draw();
--- ts-molude/main.ts
+++
@@ -1,5 +0,0 @@
-//Importing 'Point' class from './point' which is the name of the module or the path
-import { Point } from './point';
-
-let point = new Point(2,3);
-point.draw();
